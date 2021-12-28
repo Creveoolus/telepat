beb7219ac26dc5fa5a6dc700f730e45602566d34
Creveoolus <stepanholmi@gmail.com>
2021-12-28 12:24:52 +0100

    .

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import plugins
 from kivy.animation import Animation
 from kivy.app import App
 from kivy.clock import Clock
-# from kivy.config import Config
+from kivy.config import Config
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import DictProperty
+import time
+import plugins
 
 plugins.load_files()
-'''
 # Set Config
 Config.set('kivy', 'pause_on_minimize', 1)
 Config.set('kivy', 'log_dir', 'logs')
@@ -21,41 +23,60 @@ Config.set('graphics', 'height', 490)
 Config.set('graphics', 'width', 829)
 Config.set('graphics', 'resizable', 1)
 Config.write()
-'''
-Builder.load_file('telepat.kv')
 
 
 class WindowManager(ScreenManager):
-    pass
+	pass
 
 
-class LoadingWindow(Screen):
-    pass
+# class LoadingWindow(Screen):
+# 	sm = DictProperty(None, allownone=True)
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+# 		self.sm = App.get_running_app().root
+# 		self.bind(on_sm=self.execute)
+
+# 	def execute(self, *largs):
+# 		self.sm.current = 'login'
 
 
 class LoginWindow(Screen):
-    def on_kv_post(self, *largs):
-        Clock.schedule_once(self.anim_heading, 2)
+	def on_kv_post(self, *largs):
+		Clock.schedule_once(self.anim_heading, 2)
 
-    def anim_heading(self, dt):
-        heading = self.ids.heading
-        tex0, tex1 = heading.texture_size
+	def anim_heading(self, _):
+		heading = self.ids.heading
 
-        anim = Animation(
-            texture_size=(tex0 - 20, tex1 - 20),
-            duration=2.
-        )
-        # self.ids.heading.text = "test"
+		tex0, tex1 = heading.texture_size
+		anim = Animation( # animate fadein/opacity
+			opacity=1.,
+			duration=.7
+		) + Animation( # wait
+			opacity=1.,
+			duration=.5
+		) + Animation( # animate position
+			pos_hint={"top": 0.9},
+			duration=.6
+		) + Animation( # wait
+			opacity=1.,
+			duration=.2
+		) + Animation( # animate size
+			texture_size=(tex0 / 1.3, tex1 / 1.3),
+			duration=.3
+		)
 
-        anim.start(heading)
+		anim.start(heading)
 
+	def anim_login_form(self, _):
+		pass
 
-class TelepatMessengerApp(App):
-    def build(self):
-        self.root = WindowManager()
+with open('telepat.kv', encoding='utf8') as fd:
+	kv = Builder.load_string(fd.read())
 
-        return self.root
+class TelepatApp(App):
+	def build(self):
+		return kv
 
 
 if __name__ == '__main__':
-    TelepatMessengerApp().run()
+	TelepatApp().run()
--- plugins.py
+++ plugins.py
@@ -1,62 +1,74 @@
 import json
 
 dirs = {
-    "config": "assets/data/config.json",
-    "settings": "assets/data/settings.json"
+	"config": "assets/data/config.json",
+	"settings": "assets/data/settings.json"
 }
 
 
 def load_files():
-    try:
-        with open(dirs["config"], "r") as f:
-            global config
+	try:
+		with open(dirs["config"], "r", encoding='utf8') as f:
+			global config
 
-            config = json.load(f)
+			config = json.load(f)
 
-            f.close()
+			f.close()
 
-        with open(dirs["settings"], "r") as f:
-            global settings
-            settings = json.load(f)
+		with open(dirs["settings"], "r", encoding='utf8') as f:
+			global settings
+			settings = json.load(f)
 
-            f.close()
+			f.close()
 
-        with open(f"assets/themes/{settings['theme']}.json") as f:
-            global theme
-            theme = json.load(f)
+		with open(f"assets/themes/{settings['theme']}.json", "r", encoding='utf8') as f:
+			global theme
+			theme = json.load(f)
 
-            f.close()
+			f.close()
 
-        return True
+		with open(f"assets/lang/{settings['lang']}.json", "r", encoding='utf8') as f:
+			global lang
+			lang = json.load(f)
 
-    except Exception:
-        raise
+			f.close()
+
+		return True
+
+	except Exception:
+		raise
 
 
 def reload_file(file):
-    try:
-        if file == "config":
-            with open(dirs["config"]) as f:
-                config = json.load(f)  # noqa
+	try:
+		if file == "config":
+			with open(dirs["config"], "r", encoding='utf8') as f:
+				config = json.load(f)  # noqa
 
-                f.close()
+				f.close()
 
-                return True
-        elif file == "settings":
-            with open(dirs["settings"], "r") as f:
-                settings = json.load(f)
+				return True
+		elif file == "settings":
+			with open(dirs["settings"], "r", encoding='utf8') as f:
+				settings = json.load(f)
 
-                f.close()
+				f.close()
 
-                return True
-        elif file == "theme":
-            with open(f"assets/themes/{settings['theme']}.json", "r") as f:
-                theme = json.load(f)  # noqa
+				return True
+		elif file == "theme":
+			with open(f"assets/themes/{settings['theme']}.json", "r", encoding='utf8') as f:
+				theme = json.load(f)  # noqa
 
-                f.close()
+				f.close()
 
-                return True
-        else:
-            raise
-    except Exception:
-        return False
+				return True
+		elif file == "lang":
+			with open(f"assets/lang/{settings['lang']}.json", "r", encoding='utf8') as f:
+				global lang
+				lang = json.load(f)
+
+				f.close()
+		else:
+			raise
+	except Exception:
+		return False
